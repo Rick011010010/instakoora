1e84d5aefe395fe0d537244508a0693ec6326ff3
Rick011010010 <oussama.elmyar92@gmail.com>
2022-09-15 00:59:37 +0100

    route check1

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,12 @@
-
+import { useState } from 'react';
 
 function Login() {
+
+    const [register, setRegister] = useState(false)
+
+    const registerHandler = () => {
+        setRegister(!register)
+    }
 
 
     return (
@@ -20,14 +26,33 @@ function Login() {
                     <button className='bg-[#9CBA5C] border rounded-full text-white px-10 md:py-2 py-1 shadow-ms hover:shadow-xl active:scale-90 transition duration-150 md:w-56 '>Sigin</button>
                     <div className='flex justify-between mt-1 '>
                         <button className="md:pr-16 text-gray-200 md:text-zinc-700 lg:text-zinc-700 xl:text-gray-200   underline-offset-1 font-semibold">Recover password!</button>
-                        <button className='text-gray-200 md:text-zinc-700 lg:text-zinc-700 xl:text-gray-200  font-semibold '>Signup!</button>
+                        <button className='text-gray-200 md:text-zinc-700 lg:text-zinc-700 xl:text-gray-200  font-semibold ' onClick={registerHandler}>Signup!</button>
                     </div>
+                </div>
+
+                <div className={register? "  2xl:absolute 2xl:top-64 2xl:h-56  2xl:flex 2xl:flex-col 2xl:justify-around py-1 w-[100%]": " hideen"}>
+                    <div className=" flex flex-row justify-around">
+                        <label htmlFor="" className="">Enter a Valid Email</label>
+                        <input type="text" className=" rounded-2xl py-2 " />
+
+                    </div>
+
+                    <div className=" flex flex-row justify-around">
+                        <label htmlFor="" className="">Enter Password</label>
+                        <input type="password" className=" rounded-2xl py-2" />
+                    </div>
+                    <div className=" flex flex-row justify-around">
+                        <label htmlFor="">Repeat Password</label>
+                        <input type="password" className=" rounded-2xl py-2" />
+                    </div>
+
+
                 </div>
 
 
             </div>
 
-            
+
         </div>
 
 
