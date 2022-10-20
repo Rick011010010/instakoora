3c29321028da4745793ae483661ee63ee6351526
Rick011010010 <oussama.elmyar92@gmail.com>
2022-10-20 12:28:50 +0100

    teams render

--- components/MainProfil.js
+++ components/MainProfil.js
@@ -301,6 +301,7 @@ export default function MainProfil({ players }) {
 
 
   console.log(myteam, "ggggggggggggggggggggggggggggggggggggggg ")
+  console.log(teams, "cccccccccccccccccccccccc ")
 
   const [URL, setUrl] = useState('')
 
