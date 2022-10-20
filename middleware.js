c30d42065d13a99e4382475aa27d739fe0028bd2
Rick011010010 <oussama.elmyar92@gmail.com>
2022-10-20 12:20:19 +0100

    middeleware4

--- middleware.js
+++ middleware.js
@@ -1,38 +1,20 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-import { withAuth } from "next-auth/middleware"
-
-// More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      // // `/admin` requires admin role
-      // if (req.nextUrl.pathname === "/admin") {
-      //   return token?.userRole === "admin"
-      // }
-      // `/me` only requires the user to be logged in
-      return !!token
-    },
-  },
-})
-
-export const config = { matcher: ["/home"] }
 
 
+export async function middleware(req) {
+  const { pathname, origin } = req.nextUrl;
 
-// export async function middleware(req) {
-//   const { pathname, origin } = req.nextUrl;
+  if (pathname === '/home') {
+    const session = await getToken({
+      req,
 
-//   if (pathname === '/home') {
-//     const session = await getToken({
-//       req,
-
-//       secret: process.env.JWT_SECRET,
-//       secureCookie: process.env.NODE_ENV === 'production',
-//     });
-//     if (!session) return NextResponse.redirect(`${origin}/`);
-//   }
+      secret: process.env.JWT_SECRET,
+      secureCookie: process.env.NODE_ENV === 'production',
+    });
+    if (!session) return NextResponse.redirect(`${origin}/`);
+  }
 
 //   if (pathname === '/') {
 //     const session = await getToken({
@@ -48,4 +30,4 @@ export const config = { matcher: ["/home"] }
 //     if (session) return NextResponse.redirect(`${origin}/home`);
 //   }
 
-// }
+}
