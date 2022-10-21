46b9d5cf5572edc491a75d259864140414b264b7
Rick011010010 <oussama.elmyar92@gmail.com>
2022-10-21 17:36:20 +0100

    teams get

---
+++ pages/api/allteams.js/[id].js
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "../../../util/mongodb";
+import { ObjectId } from "mongodb";
+
+
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  const { db } = await connectToDatabase();
+
+  if (method === "PUT") {
+    try {
+      await db.collection("teams").fieldToUpdate({ _id: new ObjectId(id) },req.body);
+      res.status(200).json({ message: "your team card has been updated!!" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+} 
