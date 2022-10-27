ce147261a797c1b389d0d66cb6627d629e2d59b0
Rick011010010 <oussama.elmyar92@gmail.com>
2022-10-27 18:53:26 +0100

    google maps

--- components/MapLocation.js
+++ components/MapLocation.js
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import { useState, useEffect } from 'react'
+import { GiCogLock } from "react-icons/gi";
 
 export default function Home() {
     const { isLoaded } = useLoadScript({
@@ -14,6 +15,10 @@ export default function Home() {
 function Map() {
     const [posLat, setPosLat] = useState(0)
     const [posLong, setPosLong] = useState(0)
+    const [position, setPosition] = useState(0)
+    console.log(posLat, "hhhhhhfhfhhfhf")
+    console.log(posLong, "mmmmmmmmmmm")
+    console.log(position, "position")
 
 
     useEffect(() => {
@@ -23,12 +28,26 @@ function Map() {
 
             setPosLat(position.coords.latitude)
             setPosLong(position.coords.longitude)
+            setPosition(position)
 
         });
 
 
+
     }, [])
-    const center = useMemo(() => ({ lat: posLat, lng: posLong }), []);
+    const google_map_pos = new google.maps.LatLng(posLat, posLong);
+    console.log(google_map_pos, "google_map_position")
+    var google_maps_geocoder = new google.maps.Geocoder();
+    google_maps_geocoder.geocode(
+        { 'latLng': google_map_pos },
+        function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results[0]) {
+                console.log(results[0].formatted_address);
+            }
+            console.log(results, "results");
+        }
+    );
+    const center = ({ lat: posLat, lng: posLong })
 
     return (
         <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
