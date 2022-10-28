8d4bdb69021d696a0a29dbef73bd74036d9970c1
Rick011010010 <oussama.elmyar92@gmail.com>
2022-10-28 14:18:29 +0100

    google maps

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,7 @@ import { signOut, useSession } from "next-auth/react";
 function Sidebar({ articles }) {
 
     const [position, setPosition] = useState([])
-    console.log(position[0].localisation,"sssssssssssssssssssss")
+    console.log(position[0],"sssssssssssssssssssss")
 
     
 
@@ -59,7 +59,7 @@ function Sidebar({ articles }) {
                         <svg className="h-6 w-6 fill-current" viewBox="0 0 512 512">
                             <path d="M256 32c-88.004 0-160 70.557-160 156.801C96 306.4 256 480 256 480s160-173.6 160-291.199C416 102.557 344.004 32 256 32zm0 212.801c-31.996 0-57.144-24.645-57.144-56 0-31.357 25.147-56 57.144-56s57.144 24.643 57.144 56c0 31.355-25.148 56-57.144 56z" />
                         </svg>
-                        <h1 className="px-2 text-sm">{position[0].localisation}</h1>
+                        <h1 className="px-2 text-sm">ma</h1>
                     </div>
                     <div className="flex items-center mt-4 text-gray-700">
                         <svg className="h-6 w-6 fill-current" viewBox="0 0 512 512">
